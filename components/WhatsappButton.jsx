5275881a04d9ab66c62c9703ee0bfd97e49f5a1f
Globalscholarhub <visa.tripmaker@gmail.com>
2025-11-30 22:04:30 +0530

    Create WhatsappButton.jsx

---
+++ components/WhatsappButton.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function WhatsAppButton() {
+  return (
+    <a
+      href="https://wa.me/919999999999"
+      target="_blank"
+      className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-xl hover:bg-green-600 transition"
+    >
+      💬
+    </a>
+  );
+}
